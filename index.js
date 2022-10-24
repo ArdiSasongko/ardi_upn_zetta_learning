5431dc5e800f71eefd7017d2aff005e00e247783
ArdiSasongko <ardisasongko002@gmail.com>
2022-10-24 17:26:09 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function perhitungan(buku,diskon,pajak){
-    let result = 0
+function perhitungan(buku,diskon,pajak,amount_of_stock,amount_of_purchased){
+    let total = 0
     let harga = buku.Harga
     const amount_of_diskon = harga*(diskon/100)
     const HargaSetelahDiskon = harga - amount_of_diskon
@@ -13,7 +13,24 @@ function perhitungan(buku,diskon,pajak){
     console.log("Harga Pajak : ", amount_of_task)
     console.log("Harga Setelah Pajak : ", HargaSetelahPajak)
     console.groupEnd()
-    return result
+    
+    for(let i = 0; i < amount_of_stock;i++){
+        if(amount_of_purchased>amount_of_stock){
+            console.group()
+            console.log("Pembelian Melebihi Stok")
+            console.groupEnd()
+            break;
+        }
+        else{
+            total = amount_of_purchased*HargaSetelahPajak
+            console.group()
+            console.log("Pembelian masih tercukupi")    
+            console.groupEnd()
+        }
+    }
+    console.group()
+    console.log("Total : ", total)
+    console.groupEnd()
 }
 
-console.log(perhitungan({title:'Buku Baru',Harga:100000},20,5))
+console.log(perhitungan({title:'Buku Baru',Harga:100000},20,5,5,6))
