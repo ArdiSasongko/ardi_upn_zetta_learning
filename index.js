c530b8e7b9ab4f25f62a2bcd638c6dd49417791e
ArdiSasongko <ardisasongko002@gmail.com>
2022-10-26 17:31:28 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-function perhitungan(buku,diskon,pajak,amount_of_stock,amount_of_purchased){
+//object buku
+const book = [
+    {nama : "Buku baru", harga : 100000, produksi : true, stok : 10},
+    {nama : "Buku lama", harga : 90000, produksi : true, stok : 2},
+    {nama : "Buku Mainan", harga : 150000, produksi : false, stok : 1}
+]
+
+let diskon = 20
+let pajak = 5
+let interes = 3
+let pricepermonth = 0
+
+function pembelianBuku(namabuku,jumlahbuku){
+    const result = book.find(({nama})=> nama === namabuku)
+    if(result != undefined){
+        const totaldiskon = result.harga*(diskon/100)
+        const setelahdiskon = result.harga-totaldiskon
+        const totalpajak = setelahdiskon*(pajak/100)
+        const setelahpajak = setelahdiskon-totalpajak
+        const jumlahpembelian = setelahpajak*jumlahbuku
+        calCredit(jumlahpembelian)
+        console.group();
+        console.log("Stok : ", result.stok)
+        if(result.stok >= jumlahbuku){
+            console.log("Stok Tersedia")
+            console.log("Harga per Buku : ", result.harga)
+            console.log("Harga Diskon : ", totaldiskon)
+            console.log("Harga Setelah Diskon : ", setelahdiskon)
+            console.log("Harga Pajak : ", totalpajak)
+            console.log("Harga Setelah Pajak : ", setelahpajak)
+            console.log("Harga yang harus dibayarkan : ", jumlahpembelian)
+            console.log()
+            console.log("Price to pay with credit due every month ", pricepermonth)
+            console.groupEnd()
+        }
+        else{
+            console.log("Stok habis atau tidak mencukupi")
+        }
+    }
+}
+
+function calCredit(jumlahpembelian){
+    pricepermonth = jumlahpembelian + (jumlahpembelian*(interes/100))
+    return pricepermonth
+}
+
+/* function perhitungan(buku,diskon,pajak,amount_of_stock,amount_of_purchased){
     let total = 0
     let harga = buku.Harga
     const amount_of_diskon = harga*(diskon/100)
@@ -31,6 +77,6 @@ function perhitungan(buku,diskon,pajak,amount_of_stock,amount_of_purchased){
     console.group()
     console.log("Total : ", total)
     console.groupEnd()
-}
+} */
 
-console.log(perhitungan({title:'Buku Baru',Harga:100000},20,5,5,6))
+console.log(pembelianBuku("Buku baru", 4))
