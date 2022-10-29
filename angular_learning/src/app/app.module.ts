4a35f7e13d0d321691b911a0ab3b85116e2924e9
ArdiSasongko <ardisasongko002@gmail.com>
2022-10-30 01:34:14 +0700

    add module and component

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './module-1/component1/component1.component';
+import { Component2Component } from './module-1/component2/component2.component';
+import { Component3Component } from './module-1/component3/component3.component';
+import { Component4Component } from './module-1/component4/component4.component';
+import { Component5Component } from './module-1/component5/component5.component';
+import { Component6Component } from './module-1/component6/component6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component,
+    Component5Component,
+    Component6Component
   ],
   imports: [
     BrowserModule
