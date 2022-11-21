230e8e484fd9c20928624d2f8b6100c4ac2c3361
ArdiSasongko <ardisasongko002@gmail.com>
2022-11-21 15:31:47 +0700

    angular day 2

--- angular_learning/src/app/app.component.ts
+++ angular_learning/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { DataService } from './service/data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'angular_learning';
+  dataSource: any;
+  keyfilter:any='';
+  key:any;
+  data:any;
+  displayedColumns: string[] = ['Name','Usertype','Email','Status'];
+  constructor(private dataService:DataService){}
+
+  ngOnInit():void{
+    this.key = this.keyfilter;
+    this.dataService.getData().subscribe((data)=>{
+      this.dataSource=data
+    })
+  }
 }
--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { TruthPipe } from './pipe/truth.pipe';
+import { SearchPipe } from './pipe/search.pipe';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TruthPipe,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatTableModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
